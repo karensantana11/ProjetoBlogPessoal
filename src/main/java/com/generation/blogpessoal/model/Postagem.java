35c6b85ea8270c41e9b06864ce4bafc66e3b1aee
karensantana11 <133985203+karensantana11@users.noreply.github.com>
2023-06-27 16:02:52 -0300

    Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -2,12 +2,16 @@ package com.generation.blogpessoal.model;
 
 import java.time.LocalDateTime;
 
+
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -16,25 +20,63 @@ import jakarta.validation.constraints.Size;
 @Table(name = "tb_postagens")
 public class Postagem {
 
-	public @interface java {
-
-	}
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotBlank
-	@Size(min = 3, max = 100)	
+	@Size(min = 3, max = 100)
 	private String titulo;
-	
-	
+
 	@NotBlank
 	@Size(min = 10, max = 1000)
 	private String texto;
 	
-	@UpdateTimestamp
+		@UpdateTimestamp
 	private LocalDateTime data;
-	
+		
+		@ManyToOne		
+		@JsonIgnoreProperties("Postagem")
+	     private Tema tema;
+
+	public Long getId() {
+		return id;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+    public Tema getTema() {
+    	return tema;
+    }
+    
+    public void setTema(Tema tema) {
+    	this.tema = tema;
+    }
+    
+    }
